e8c5229eda606c3adad9fc7385bf43ea7f227ce1
Manoj Manchuri <manchurimanoj4@gmail.com>
2025-08-03 05:26:23 +0000

    use hackerrank logo for displaying hackerrank link in the footer page

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -3,6 +3,19 @@ import { Github, Linkedin, Code } from "lucide-react"
 import { socialLinks } from "@/lib/data"
 import { Button } from "@/components/ui/button"
 
+const HackerRankIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+      role="img"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      {...props}
+    >
+      <title>HackerRank</title>
+      <path d="M12.012 24a12 12 0 1 1 0-24 12 12 0 0 1 0 24zM9.162 15.607V12.55h-2.313v3.057h2.313zm5.701 0V12.55h-2.313v3.057h2.313zM9.09 11.463h2.52V8.41H9.09v6.04h2.52v-2.977h.072v2.977h2.52V8.41h-2.52v2.977h-.072V8.41H9.09z" />
+    </svg>
+  );
+
 export function AppFooter() {
   return (
     <footer className="bg-background border-t">
@@ -28,7 +41,7 @@ export function AppFooter() {
           </Button>
           <Button variant="ghost" size="icon" asChild>
             <Link href={socialLinks.hackerrank} target="_blank" rel="noopener noreferrer" aria-label="HackerRank">
-                <Code className="h-5 w-5" />
+                <HackerRankIcon className="h-5 w-5" />
             </Link>
           </Button>
         </div>
